f19ed03cc3807daa18f111c0a78d1068918ec400
Vergosss <72676680+Vergosss@users.noreply.github.com>
2026-06-10 20:12:13 +0300

    Evaluating benchmarks on the whole set.
    More compact and cleaner code.

--- Benchmarking/Benchmark_Inference.py
+++ Benchmarking/Benchmark_Inference.py
@@ -3,14 +3,14 @@ from peft import PeftModel, PeftConfig
 import torch
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 #
+######################PER STEP LATENCIES/THROUGHPUTS ON A DISTRIBUTED SETUP############
 
 latencies = []
 throughputs = []
 #
-class Benchmarking_per_Epoch(TrainerCallback):
-
+class Benchmarking(TrainerCallback):
+'''
     def on_epoch_begin(self,args,state,control,**kwargs):
         torch.cuda.synchronize()
         self.start_time = time.perf_counter()
@@ -31,6 +31,8 @@ class Benchmarking_per_Epoch(TrainerCallback):
       #mean time / batch , batch1 n1 secs,batch2 n2 secs etc
       latencies.append(step_latency)
       throughputs.append(step_throughput)
+'''
+
 
 
 #
@@ -56,7 +58,7 @@ print('Num labels:',lora.config.num_labels)
 ##
 lora.eval() ###Evaluation mode since we are running inference
 
-####################
+##########Load Data##########
 
 event_traces = pd.read_csv('/storage/data2/up1072604/data/Event_traces.csv',usecols=['BlockId','Label','Features'])
 print('CHECK:',event_traces.index.duplicated().any())
@@ -70,47 +72,26 @@ event_traces.rename(columns={'Features':'text','Label':'label'}, inplace=True) #
 event_traces.info()
 ###Dictionary of EventIds-Event text###
 event_dictionary = dict(zip(log_templates['EventId'],log_templates['EventTemplate']))
-###Train-val-test split- Get the test set the same set from training script by setting same random state###
-event_traces_train,event_traces_test = train_test_split(event_traces,test_size=0.1,stratify=event_traces['label'],random_state=42,shuffle=True)
-event_traces_train,event_traces_validation = train_test_split(event_traces_train,test_size=0.1111,stratify=event_traces_train['label'],random_state=42,shuffle=True)
-###Sample from the test set###
+
 ###Apply on each row of the dataset###
 def features_to_strings(entry):
   return " ".join([event_dictionary.get(eventID) for eventID in entry['text'].replace('[','').replace(']','').split(',')])
 ##
-event_traces_train['text'] = event_traces_train.apply(features_to_strings,axis=1)
-event_traces_validation['text'] = event_traces_validation.apply(features_to_strings,axis=1)
-event_traces_test['text'] = event_traces_test.apply(features_to_strings,axis=1)
-print(event_traces_train.sample(1))
+print(event_traces.sample(1))
 
 ######################
 ###Number of distinct labels in dataset###
 no_of_labels = int(event_traces['Label'].nunique())
-###Calculate class weights with the inverse class frequency(inverse of each class percentage in the train dataset)##
-weights = event_traces_train['Label'].value_counts(normalize=True)
+###Calculate class weights with the inverse class frequency(inverse of each class percentage in the dataset)##
+weights = event_traces['Label'].value_counts(normalize=True) #same as if i had splittd because the split was stratified, percentage would be the same
 weights = torch.tensor([1/weights.loc[x] for x in sorted(list(weights.index))])
 print(weights)
 ##################
 
-'''
-OR ON THE WHOLE DATASET:
 event_traces['text'] = event_traces.apply(features_to_strings,axis=1)
 event_traces = Dataset.from_pandas(event_traces)
-'''
+event_traces = event_traces.map(tokenize_logs,batched=True)
 
-
-
-###Convert to Huggingface Dataset###
-event_traces_train = Dataset.from_pandas(event_traces_train)
-event_traces_test = Dataset.from_pandas(event_traces_test)
-event_traces_validation = Dataset.from_pandas(event_traces_validation)
-
-##############
-###Tokenizing###
-event_traces_train = event_traces_train.map(tokenize_logs,batched=True)
-event_traces_test = event_traces_test.map(tokenize_logs,batched=True)
-event_traces_validation = event_traces_validation.map(tokenize_logs,batched=True)
-###########################
 
 ###FOCAL LOSS FUNCTION###
 class SparseCategoricalFocalLoss(nn.Module):
@@ -148,9 +129,6 @@ class ImbalancedTrainer(Trainer):
 		#
 		return (loss,outputs) if return_outputs else loss
         
-        
-        
-        
 ###################################
 training_arguments = TrainingArguments(
     output_dir = '/storage/data2/up1072604/run', #Location where the fine tuned model's weights will be stored
@@ -161,28 +139,20 @@ training_arguments = TrainingArguments(
 trainer = ImbalancedTrainer(
     model=lora, #The model
     args=training_arguments, #Training arguments
+    callbacks = [Benchmarking()]
  )
 
-results = trainer.evaluate(eval_dataset=event_traces_test) #Evaluate on unseen test subset
-###########################--------INFERENCE/EVALUATING ON A TEST SET ##########
+###########################--------INFERENCE/EVALUATING ON THE WHOLE  SET ##########
 latencies = []
 torch.cuda.synchronize() #leftover gpu work from earlier
 start = time.perf_counter()
-eval_results = trainer.evaluate(eval_dataset=event_traces_test)
+eval_results = trainer.evaluate(eval_dataset=event_traces)
 torch.cuda.synchronize()
 print(f'WALL CLOCK INFERENCE/EVALUATION TIME :{time.perf_counter() - start:.2f}') #### STOP COUNTING AFTER EVALUATION
 print(f'Global Latency(Trainer): {results.metrics['eval_runtime']:.2f}')
-print(f'Global Throughput defined as N_samples/time it took for these samples: {event_traces_test)/results.metrics['eval_runtime']:.2f}')
+print(f'Global Throughput defined as N_samples/time it took for these samples: {len(event_traces)/results.metrics['eval_runtime']:.2f}')
 print(f'Global Throughput(Trainer): {results.metrics['eval_samples_per_second']:.2f}')
 
 ##
 input('WAIT')
-######Average batch metrics##############
-
-print(f'Average Batch/step latency: {sum(latencies)/len(latencies):.2f} seconds per batch/step')
-print(f'Average Batch/step throughput: {sum(throughputs)/len(throughputs):.2f} samples per second')
-#######Global Trainer metrics#####
-print(f'Global Throughput(Trainer): {results.metrics['eval_samples_per_second']:.2f} vs Mean of Throughputs: {sum(throughputs)/len(throughputs):.2f}') #Close due to low latencies variance
-print(f'Global Latency(Trainer): {results.metrics['eval_runtime']:.2f} vs Sum of Latencies: {sum(latencies):.2f}')
-########Global Metrics ################
-print(f'Global Throughput(total samples/trainer runtime): {len(event_traces_test)/results.metrics['eval_runtime']:.2f}')
+######AVERAGE STEP METRICS##############
